085bc521a76395651721dda0ead6aefe493aaaaa
Imash <imashkoksal@gmail.com>
2023-10-30 11:58:37 +0600

    asdasda

---
+++ lib/ai.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class _AIHelper extends StatefulWidget {
+  const _AIHelper({super.key});
+
+  @override
+  State<_AIHelper> createState() => __AIHelperState();
+}
+
+class __AIHelperState extends State<_AIHelper> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("skibididopdop"),
+    );
+  }
+}
---
+++ lib/navbar.dart
@@ -0,0 +1,43 @@
+import "package:flutter/material.dart";
+import 'package:nishack2023/ai.dart';
+
+class NavBar extends StatefulWidget {
+  const NavBar({super.key});
+
+  @override
+  State<NavBar> createState() => _NavBarState();
+}
+
+class _NavBarState extends State<NavBar> {
+  int currentIndex = 0;
+  final screens = [
+    _AIHelper(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: screens[currentIndex],
+      bottomNavigationBar: SizedBox(
+        height: 70,
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.shifting,
+          selectedItemColor: Colors.white,
+          backgroundColor: const Color.fromARGB(255, 249, 114, 3),
+          currentIndex: currentIndex,
+          onTap: (index) => setState(() {
+            currentIndex = index;
+          }),
+          items: const[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+           label: "Home",
+           backgroundColor: Color.fromARGB(255, 249, 114, 3)),
+             BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_rounded),
+             label: "Profile",
+             backgroundColor: Color.fromARGB(255, 249, 114, 3)),
+        ]),
+      ),
+    );
+  }
+}
